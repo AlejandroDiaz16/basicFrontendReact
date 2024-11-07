3fca3686212d42e1e96de05e4b1a2104573d2717
AlejandroDiaz16 <sergiobustos69@gmail.com>
2024-11-06 21:45:46 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,21 @@
+const API_BASE_URL = 'http:localhost:5000/api'
+
+async function apiFetch(endpoint, { method = 'GET', headers = {}, body = null} = {}) {
+    const config = {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            ...headers
+        }
+    }
+
+    if (body) {
+        config.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, config);
+
+    return response.json();
+}
+
+export default apiFetch;
